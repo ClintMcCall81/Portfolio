ac39db56ba64598b1abac59d62d1a40df6b36eb0
clintmccall81 <clintmccall81@gmail.com>
2021-04-07 22:35:39 -0400

    CSS Style Change

--- contactForm.php
+++ contactForm.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
 
-    $mailTo = "clint.mccall@georgebrown.ca";
+    $mailTo = "mccall_clint@hotmail.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an email from ".$name.".\n\n".$message;
 

0b85531eed571c260f82e0ed3e0dd875cff365f0
clintmccall81 <clintmccall81@gmail.com>
2021-04-12 00:44:39 -0400

    Multiple index.html and css additions and changes.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// FILTERED PHOTO GRID
 $(document).ready(function(){
     $('.list').click(function(){
         const value = $(this).attr('data-filter');
@@ -14,3 +15,22 @@ $(document).ready(function(){
         $(this).addClass('active').siblings().removeClass('active');
     })
 })
+
+// MODAL JS
+let modalBtn = document.getElementById("modal-musicApp")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
